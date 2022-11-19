5cbf0cce2d25ab18bef5a4e522cd2e8d75567163
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-11-18 21:57:14 -0800

    Add IAM policies to EC2

--- workspaces/mastodon/iam.tf
+++ workspaces/mastodon/iam.tf
@@ -1,14 +1,3 @@
-data "aws_iam_policy_document" "ec2-assume-role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
 resource "aws_iam_role" "main" {
   name               = local.name
   assume_role_policy = data.aws_iam_policy_document.ec2-assume-role.json
@@ -21,5 +10,47 @@ resource "aws_iam_instance_profile" "main" {
 
 resource "aws_iam_role_policy_attachment" "s3" {
   role       = aws_iam_role.main.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  policy_arn = data.terraform_remote_state.s3.outputs.s3-main.iam_policy_rw
+}
+
+resource "aws_iam_role_policy" "main" {
+  role   = aws_iam_role.main.name
+  policy = data.terraform_remote_state.s3.outputs.s3-main.iam_policy_rw
+}
+
+data "aws_iam_policy_document" "main" {
+  # Allow reading from parameter store
+  # https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-paramstore-access.html
+  statement {
+    actions = [
+      "ssm:PutParameter",
+      "ssm:DeleteParameter",
+      "ssm:GetParameterHistory",
+      "ssm:GetParametersByPath",
+      "ssm:GetParameters",
+      "ssm:GetParameter",
+      "ssm:DeleteParameters"
+    ]
+    resources = [
+      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:parameter/${local.name}/*"
+    ]
+  }
+  statement {
+    actions = [
+      "ssm:DescribeParameters",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+  # Allow sending emails
+  statement {
+    actions = [
+      "ses:SendEmail",
+      "ses:SendRawEmail",
+    ]
+    resources = [
+      "*"
+    ]
+  }
 }

d8c1d8733c5d23361d24f420923004b657d81f10
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-11-18 19:27:19 -0800

    Add LB

---
+++ workspaces/mastodon/lb.tf
@@ -0,0 +1,42 @@
+resource "aws_lb" "main" {
+  name_prefix = local.name
+  subnets     = data.terraform_remote_state.vpc.outputs.subnet_ids.public[*]
+}
+
+resource "aws_lb_listener" "main_http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "main_https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = ""
+  }
+}
+
+resource "aws_lb_target_group" "main" {
+  name_prefix = local.name
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "main" {
+  target_group_arn = aws_lb_target_group.main.arn
+  target_id        = aws_instance.main.id
+}

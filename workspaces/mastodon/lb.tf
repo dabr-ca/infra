07bec01da32ba4b2c130552c75a6c2d21f13da07
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-11-18 19:35:56 -0800

    Associate target group

--- workspaces/mastodon/lb.tf
+++ workspaces/mastodon/lb.tf
@@ -26,7 +26,7 @@ resource "aws_lb_listener" "main_https" {
 
   default_action {
     type             = "forward"
-    target_group_arn = ""
+    target_group_arn = aws_lb_target_group.main.arn
   }
 }
 

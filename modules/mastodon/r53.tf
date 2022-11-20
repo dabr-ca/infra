447862b1399bfccf3310212a446590fb60eae94f
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-11-20 01:43:41 -0800

    Create CloudFront for S3 bucket

---
+++ modules/mastodon/r53.tf
@@ -0,0 +1,17 @@
+resource "aws_route53_zone" "main" {
+  name = var.domain
+}
+
+resource "aws_route53_record" "files" {
+  for_each = toset(["A", "AAAA"])
+
+  zone_id = aws_route53_zone.main.id
+  name    = "files.${var.domain}"
+  type    = each.key
+
+  alias {
+    zone_id                = aws_cloudfront_distribution.main.hosted_zone_id
+    name                   = aws_cloudfront_distribution.main.domain_name
+    evaluate_target_health = false
+  }
+}

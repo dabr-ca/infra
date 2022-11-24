fe9e96b178f69bdeb1d3d354b0fb5359a5d7b80f
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-11-23 22:08:33 -0800

    Enable media proxy on CloudFront

--- modules/pleroma/cloudfront.tf
+++ modules/pleroma/cloudfront.tf
@@ -18,12 +18,36 @@ resource "aws_cloudfront_distribution" "main" {
     origin_request_policy_id = data.aws_cloudfront_origin_request_policy.managed["CORS-S3Origin"].id
   }
 
+  ordered_cache_behavior {
+    path_pattern     = "proxy/*"
+    target_origin_id = aws_lb.main.dns_name
+
+    compress                 = true
+    viewer_protocol_policy   = "redirect-to-https"
+    allowed_methods          = ["GET", "HEAD"]
+    cached_methods           = ["GET", "HEAD"]
+    cache_policy_id          = data.aws_cloudfront_cache_policy.managed["CachingOptimized"].id
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.managed["CORS-S3Origin"].id
+  }
+
   origin {
     origin_id   = aws_s3_bucket.main.bucket_regional_domain_name
     domain_name = aws_s3_bucket.main.bucket_regional_domain_name
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.main.cloudfront_access_identity_path
+    }
+  }
+
+  origin {
+    origin_id   = aws_lb.main.dns_name
+    domain_name = data.aws_route53_zone.main.name
+
+    custom_origin_config {
+      origin_protocol_policy = "https-only"
+      http_port              = 80
+      https_port             = 443
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
 

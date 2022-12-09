cef5e531ad98bf1a61ba7ad739963e800fed6d6d
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-12-09 02:48:54 -0800

    Place EC2 in the same AZ as RDS

--- modules/pleroma/main.tf
+++ modules/pleroma/main.tf
@@ -2,10 +2,21 @@ locals {
   name = var.name
 }
 
+# Place EC2 in the same AZ as the RDS to avoid cross-AZ data charge
+data "aws_subnet" "ec2" {
+  for_each = toset(var.public_subnet_ids)
+
+  id = each.key
+  filter {
+    name   = "availability-zone"
+    values = [aws_db_instance.main.availability_zone]
+  }
+}
+
 resource "aws_instance" "main" {
   ami                  = data.aws_ami.ubuntu.id
   instance_type        = var.ec2_instance_type
-  subnet_id            = var.public_subnet_ids[0]
+  subnet_id            = one(data.aws_subnet.ec2[*].id)
   key_name             = data.aws_key_pair.main.key_name
   iam_instance_profile = aws_iam_instance_profile.main.name
 

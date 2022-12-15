a344ad921e716bfc26f6ed609f5781582ba5f713
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-12-14 23:08:14 -0800

    Replicate main instance

---
+++ workspaces/manhole/.terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "4.46.0"
+  constraints = "~> 4.9"
+  hashes = [
+    "h1:EZB4OgvytV38JpWyye9zoMQ0bfT9yB9xSXM5NY3Lrws=",
+  ]
+}
---
+++ workspaces/manhole/main.tf
@@ -0,0 +1,31 @@
+locals {
+  name = "dabr-ca"
+}
+
+data "aws_instance" "main" {
+  filter {
+    name   = "tag:Name"
+    values = [local.name]
+  }
+  filter {
+    name   = "instance-state-name"
+    values = ["running"]
+  }
+}
+
+resource "aws_instance" "manhole" {
+  ami                    = data.aws_instance.main.ami
+  instance_type          = data.aws_instance.main.instance_type
+  subnet_id              = data.aws_instance.main.subnet_id
+  key_name               = data.aws_instance.main.key_name
+  iam_instance_profile   = data.aws_instance.main.iam_instance_profile
+  vpc_security_group_ids = data.aws_instance.main.vpc_security_group_ids
+
+  root_block_device {
+    volume_type = "gp3"
+  }
+}
+
+resource "aws_eip" "manhole" {
+  instance = aws_instance.manhole.id
+}

512d8116308a3d13dfdaf0744aa1941ec740a1bd
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-12-15 00:24:47 -0800

    Add lifecycle config to backup bucket

--- modules/pleroma/s3_backup.tf
+++ modules/pleroma/s3_backup.tf
@@ -1,3 +1,7 @@
+locals {
+  pg_dump_prefix = "pgdump/"
+}
+
 resource "aws_s3_bucket" "backup" {
   bucket = "${var.name}-backup-${random_id.s3_bucket_suffix.hex}"
 }
@@ -10,6 +14,37 @@ resource "aws_s3_bucket_public_access_block" "backup" {
   ignore_public_acls      = true
   block_public_policy     = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "backup" {
+  bucket = aws_s3_bucket.backup.id
+
+  rule {
+    id     = "ArchiveDatabaseDumps"
+    status = "Enabled"
+
+    filter {
+      prefix = local.pg_dump_prefix
+    }
+
+    transition {
+      days          = 7
+      storage_class = "STANDARD_IA"
+    }
+  }
+
+  rule {
+    id     = "DeleteDatabaseDumps"
+    status = "Enabled"
+
+    filter {
+      prefix = local.pg_dump_prefix
+    }
+
+    expiration {
+      days = 365
+    }
+  }
 }
 
 resource "aws_ssm_parameter" "s3_bucket_backup" {

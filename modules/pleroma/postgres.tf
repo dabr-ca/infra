17e48aa54f284ff81fe550d3459d385778e8737b
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-12-14 20:01:01 -0800

    Tune PostgreSQL parameters

--- modules/pleroma/postgres.tf
+++ modules/pleroma/postgres.tf
@@ -34,11 +34,6 @@ resource "aws_db_subnet_group" "main" {
   subnet_ids = var.private_subnet_ids
 }
 
-resource "aws_db_parameter_group" "main" {
-  name   = local.name
-  family = "postgres13"
-}
-
 resource "random_password" "postgres" {
   length  = 16
   special = false
@@ -66,4 +61,94 @@ resource "aws_ssm_parameter" "postgres_password" {
   name  = "/${local.name}/postgres/password"
   type  = "SecureString"
   value = random_password.postgres.result
+}
+
+# Tune PostgreSQL performance
+# https://docs-develop.pleroma.social/backend/configuration/postgresql/
+# https://pgtune.leopard.in.ua/
+# DB Version: 13
+# OS Type: linux
+# DB Type: web
+# Total Memory (RAM): 1 GB
+# CPUs num: 2
+# Connections num: 20
+# Data Storage: ssd
+
+# max_connections = 20
+# shared_buffers = 256MB
+# effective_cache_size = 768MB
+# maintenance_work_mem = 64MB
+# checkpoint_completion_target = 0.9
+# wal_buffers = 7864kB
+# default_statistics_target = 100
+# random_page_cost = 1.1
+# effective_io_concurrency = 200
+# work_mem = 6553kB
+# min_wal_size = 1GB
+# max_wal_size = 4GB
+
+resource "aws_db_parameter_group" "main" {
+  name   = local.name
+  family = "postgres13"
+
+  # Sets the maximum number of concurrent connections.
+  # Default: LEAST({DBInstanceClassMemory/9531392},5000) = 112
+  parameter {
+    max_connections = 20
+  }
+  # (8kB) Sets the number of shared memory buffers used by the server.
+  # Default: {DBInstanceClassMemory/32768} = 32768
+  parameter {
+    shared_buffers = 32768
+  }
+  # (8kB) Sets the planners assumption about the size of the disk cache.
+  # Default: {DBInstanceClassMemory/16384} = 65536
+  parameter {
+    effective_cache_size = 98304
+  }
+  # (kB) Sets the maximum memory to be used for maintenance operations.
+  # Default: GREATEST({DBInstanceClassMemory*1024/63963136},65536) = 65536
+  parameter {
+    maintenance_work_mem = 65536
+  }
+  # Time spent flushing dirty buffers during checkpoint, as fraction of checkpoint interval.
+  # Default: 0.9
+  parameter {
+    checkpoint_completion_target = 0.9
+  }
+  # (8kB) Sets the number of disk-page buffers in shared memory for WAL.
+  # Default: (empty)
+  parameter {
+    wal_buffers = 983
+  }
+  # Sets the default statistics target.
+  # Default: (empty)
+  parameter {
+    default_statistics_target = 100
+  }
+  # Sets the planners estimate of the cost of a nonsequentially fetched disk page.
+  # Default: (empty)
+  parameter {
+    random_page_cost = 1.1
+  }
+  # Number of simultaneous requests that can be handled efficiently by the disk subsystem.
+  # Default: (empty)
+  parameter {
+    effective_io_concurrency = 200
+  }
+  # (kB) Sets the maximum memory to be used for query workspaces.
+  # Default: (empty)
+  parameter {
+    work_mem = 6553
+  }
+  # (MB) Sets the minimum size to shrink the WAL to.
+  # Default: 192
+  parameter {
+    min_wal_size = 1024
+  }
+  # (MB) Sets the WAL size that triggers a checkpoint.
+  # Default: 2048
+  parameter {
+    max_wal_size = 4096
+  }
 }

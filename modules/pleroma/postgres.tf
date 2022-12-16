3169e60d001b70b181f93ca8175c0422aa729085
Zhuoyun Wei <wzyboy@wzyboy.org>
2022-12-16 08:13:58 -0800

    fixup! rds: enable performance insights

--- modules/pleroma/postgres.tf
+++ modules/pleroma/postgres.tf
@@ -17,9 +17,8 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.db.id]
 
-  backup_retention_period               = 7
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
+  backup_retention_period      = 7
+  performance_insights_enabled = true
 
   username = local.postgres_username
   password = random_password.postgres.result

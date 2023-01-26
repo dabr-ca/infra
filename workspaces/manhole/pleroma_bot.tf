91a8126107c5a09d490e985fee3f45e324be317d
Zhuoyun Wei <wzyboy@wzyboy.org>
2023-01-25 22:19:36 -0800

    fixup! Merge pull request #26 from dabr-ca/DABR-15/relay

--- workspaces/manhole/pleroma_bot.tf
+++ workspaces/manhole/pleroma_bot.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "pleroma_bot_token" {
   name  = "/${local.name}/pleroma_bot_token"
-  type  = "String"
+  type  = "SecureString"
   value = "." # populate this value manually
 
   lifecycle {

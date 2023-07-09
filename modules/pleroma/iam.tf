9213ca48b688c2f21bcf4266e6316009586cbab7
Zhuoyun Wei <wzyboy@wzyboy.org>
2023-07-09 14:47:26 -0700

    fix: restrict S3 permissions

--- modules/pleroma/iam.tf
+++ modules/pleroma/iam.tf
@@ -34,7 +34,10 @@ data "aws_iam_policy_document" "main" {
   }
   statement {
     actions = [
-      "s3:*"
+      "s3:ListBucket",
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
     ]
     resources = [
       aws_s3_bucket.main.arn,

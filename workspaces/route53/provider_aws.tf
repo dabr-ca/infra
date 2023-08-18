6baa93d00815775e9ab26168e57301b1e6012909
Zhuoyun Wei <wzyboy@wzyboy.org>
2023-08-18 00:21:06 -0700

    refactor: migrate r53 records

---
+++ workspaces/route53/provider_aws.tf
@@ -0,0 +1,1 @@
+../../snippets/provider_aws.tf
---
+++ workspaces/route53/terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">= 1.2.9"
+  cloud {
+    organization = "dabr-ca"
+    workspaces {
+      name = "route53"
+    }
+  }
+}

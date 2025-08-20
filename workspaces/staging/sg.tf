c775675f54c4bd4c45e044c75b26edad80735873
Zhuoyun Wei <wzyboy@wzyboy.org>
2025-08-20 08:50:22 -0700

    update sg

--- workspaces/staging/sg.tf
+++ workspaces/staging/sg.tf
@@ -9,14 +9,12 @@ resource "aws_vpc_security_group_ingress_rule" "backend_ssh" {
   from_port         = 22
   to_port           = 22
   cidr_ipv4         = "0.0.0.0/0"
-  description       = "SSH from everywhere"
 }
 
 resource "aws_vpc_security_group_egress_rule" "backend_vpc" {
   security_group_id = aws_security_group.backend.id
   ip_protocol       = "all"
-  cidr_ipv4         = "10.0.0.0/8"
-  description       = "All to VPC"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_security_group" "db" {
@@ -28,5 +26,4 @@ resource "aws_vpc_security_group_ingress_rule" "db_vpc" {
   security_group_id = aws_security_group.db.id
   ip_protocol       = "all"
   cidr_ipv4         = "10.0.0.0/8"
-  description       = "All from VPC"
 }

a23b5d14663d21aac30960e027170d73d66872e3
Zhuoyun Wei <wzyboy@wzyboy.org>
2025-08-22 16:18:46 -0700

    feat: forward /metrics from 443/tcp to 4021/tcp

---
+++ modules/pleroma/metrics.tf
@@ -0,0 +1,36 @@
+resource "aws_lb_target_group" "metrics" {
+  name     = "${local.name}-metrics"
+  port     = 4021
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    enabled = true
+    path    = "/metrics"
+    matcher = "200,401"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "metrics" {
+  target_group_arn = aws_lb_target_group.metrics.arn
+  target_id        = aws_instance.main.id
+}
+
+resource "aws_lb_listener_rule" "metrics" {
+  listener_arn = aws_lb_listener.main_https.arn
+
+  condition {
+    host_header {
+      values = [var.domain]
+    }
+  }
+  condition {
+    path_pattern {
+      values = ["/metrics"]
+    }
+  }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.metrics.arn
+  }
+}

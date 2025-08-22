d54d19d816a7e71aee7d7ca1a4d81f4924a2d8b1
Zhuoyun Wei <wzyboy@wzyboy.org>
2025-08-22 16:05:33 -0700

    Revert "feat: create promethus token for Grafana Cloud"
    This reverts commit 7e2e99b90313e0962e890b8a2b0a7bfda818450e.

--- workspaces/pleroma/grafana.tf
+++
@@ -1,9 +0,0 @@
-# Integration with Grafana Cloud
-
-resource "random_uuid" "prometheus_token" {}
-
-resource "aws_ssm_parameter" "prometheus_token" {
-  name  = "/${local.name}/grafana/prometheus_token"
-  type  = "SecureString"
-  value = random_uuid.prometheus_token.result
-}

35f77d710288a13aaf59fe6787d882a6ec93b9d3
Zhuoyun Wei <wzyboy@wzyboy.org>
2025-12-06 20:17:13 -0800

    fix: recon

--- workspaces/manhole/ec2.tf
+++ workspaces/manhole/ec2.tf
@@ -11,7 +11,7 @@ data "aws_instance" "main" {
 
 resource "aws_instance" "manhole" {
   ami                    = data.aws_ami.ubuntu22.id
-  instance_type          = data.aws_instance.main.instance_type
+  instance_type          = "t3.micro"
   subnet_id              = data.aws_instance.main.subnet_id
   key_name               = data.aws_instance.main.key_name
   iam_instance_profile   = data.aws_instance.main.iam_instance_profile
